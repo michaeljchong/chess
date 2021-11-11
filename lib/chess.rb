17a1f1e7c82a1efb5872e9ae5524ca658cda5108
Michael Chong <mikec0920@gmail.com>
2021-11-10 17:21:11 -0800

    Pseudocode for main game logic

---
+++ lib/chess.rb
@@ -0,0 +1,15 @@
+# Pseudocode
+# initialize a game
+#   8x8 board created
+#   chess pieces placed
+
+# Loop until game over?
+#   display the current state of the board
+#   until valid move
+#     receive current player's move
+#       select piece to move
+#       select position to move to
+#   perform player's move
+#   switch player
+
+require_relative 'board'
