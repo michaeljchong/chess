eb7b957df91b7ac64a0c6d84457f39d7bfeac5ad
Michael Chong <mikec0920@gmail.com>
2021-11-10 19:55:54 -0800

    Add unicode symbols

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -5,26 +5,44 @@ class Pieces
 end
 
 class Pawn < Pieces
-
+  def initialize(color)
+    super(color)
+    @symbol = color == 'white' ? "\u2659" : "\u265f"
+  end
 end
 
 class Rook < Pieces
-
+  def initialize(color)
+    super(color)
+    @symbol = color == 'white' ? "\u2656" : "\u265c"
+  end
 end
 
 class Knight < Pieces
-
+  def initialize(color)
+    super(color)
+    @symbol = color == 'white' ? "\u2658" : "\u265e"
+  end
 end
 
 class Bishop < Pieces
-
+  def initialize(color)
+    super(color)
+    @symbol = color == 'white' ? "\u2657" : "\u265d"
+  end
 end
 
 class Queen < Pieces
-
+  def initialize(color)
+    super(color)
+    @symbol = color == 'white' ? "\u2655" : "\u265b"
+  end
 end
 
 class King < Pieces
-
+  def initialize(color)
+    super(color)
+    @symbol = color == 'white' ? "\u2654" : "\u265a"
+  end
 end
 
