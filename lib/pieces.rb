4b86809606f1d5982ffb692a6aaf75ce6e8e76d3
Michael Chong <mikec0920@gmail.com>
2021-11-10 17:25:59 -0800

    Initialize pieces classes

---
+++ lib/pieces.rb
@@ -0,0 +1,29 @@
+class Pieces
+  @color = nil
+  
+end
+
+class Pawn < Pieces
+
+end
+
+class Rook < Pieces
+
+end
+
+class Knight < Pieces
+
+end
+
+class Bishop < Pieces
+
+end
+
+class Queen < Pieces
+
+end
+
+class King < Pieces
+
+end
+
