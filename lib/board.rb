f7ae5572dda1d6b4b5d067850fe4ed6415800fc0
Michael Chong <mikec0920@gmail.com>
2021-11-10 19:19:54 -0800

    Populate initial board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,9 @@
+require_relative 'pieces'
+
 class Board
   def initialize
     @board = Array.new(8) { Array.new (8) }
+    populate
   end
 
   def display
@@ -8,6 +11,22 @@ class Board
       p row
     end
   end
-end
 
-Board.new.display
+  def populate
+    colors = ['black', 'white']
+    row = [[0, 1], [7, 6]]
+    colors.each_with_index do |color, i|
+      @board[row[i][0]] = [
+        Rook.new(color),
+        Knight.new(color),
+        Bishop.new(color),
+        Queen.new(color),
+        King.new(color),
+        Bishop.new(color),
+        Knight.new(color),
+        Rook.new(color)
+      ]
+      @board[row[i][1]].map! { |tile| Pawn.new(color) }
+    end
+  end
+end
