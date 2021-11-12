865811e8ca40a0fa37075d71cb0776da1c099a21
Michael Chong <mikec0920@gmail.com>
2021-11-11 21:09:57 -0800

    Implement player_move method

--- lib/chess.rb
+++ lib/chess.rb
@@ -15,6 +15,23 @@ class Game
     # end
   end
 
+  def player_move
+    piece = choose_piece
+    move = choose_move
+  #   until valid move
+  #     receive current player's move
+  #       select piece to move
+  #       select position to move to
+  end
+
+  def choose_piece
+
+  end
+
+  def choose_move
+
+  end
+
   def switch_player
     @current_player = @current_player == 'white' ? 'black' : 'white'
   end
